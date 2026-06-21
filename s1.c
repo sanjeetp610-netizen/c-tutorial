6896132b74fea119d77e6bc4c5dc65135a41ef37
sanjeetp610-netizen <sanjeetp610@gmail.com>
2026-06-21 19:08:29 +0530

    Create s1.c

---
+++ s1.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, reversed = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (originalNum == reversed)
+        printf("%d is a Palindrome Number.\n", originalNum);
+    else
+        printf("%d is Not a Palindrome Number.\n", originalNum);
+
+    return 0;
+}
